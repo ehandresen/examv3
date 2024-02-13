eedc0bd6a05f73f232ca5a32efc25ab32e5ce95e
ehandresen <eirik.horgen.andresen@gmail.com>
2024-02-13 15:55:51 +0100

    added css to RaceItem

--- frontend/src/components/RaceItem.tsx
+++ frontend/src/components/RaceItem.tsx
@@ -1,17 +1,21 @@
 import { FC } from 'react';
 import { Race } from '../interfaces/IRace';
+import '../css/RaceItem.css';
 
 interface RaceItemProps {
   race: Race;
 }
 
+const imageURL = 'http://localhost:5000/images';
+
 const RaceItem: FC<RaceItemProps> = ({ race }) => {
   return (
-    <div>
+    <div className="race-item">
       <h3>Grand Prix: {race.grandPrix}</h3>
       <p>Winner: {race.winnerName}</p>
       <p>Time: {race.winnerTime}</p>
       <p>Laps: {race.numberOfLaps}</p>
+      <img src={`${imageURL}/${race.image}`} alt="" />
     </div>
   );
 };
