c0f97ca5c68e71af3767e1e2b2e48d2c8aa8cda8
ehandresen <eirik.horgen.andresen@gmail.com>
2024-02-14 16:06:52 +0100

    added POST method in RaceService

--- frontend/src/services/RaceService.ts
+++ frontend/src/services/RaceService.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import { Race } from '../interfaces/IRace';
 
 const RaceService = (() => {
   const controller = 'http://localhost:5000/api/races';
+  const imageUploadController = 'http://localhost:5000/ImageUpload';
 
   // GET
   async function getAll() {
@@ -21,10 +23,44 @@ const RaceService = (() => {
   async function getByGrandPrix(grandPrix: string) {
     try {
       const response = await axios.get(
-        `${controller}/getByGrandPrix/${grandPrix}`
+        `${controller}/GetByGrandPrix/${grandPrix}`
       );
 
       return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // POST
+  // we save the new race, and the image file attached to the object
+  async function createNewRace(newRace: Race, image: File) {
+    try {
+      /* 
+        For at IFormFile i ImageUploadController skal kunne ta imot bildet,
+        må bildet bli pakket inn i et eget type objekt (JS FormData() objektet)
+        Man kan ikke bare sende bildet uten videre 
+      */
+      await axios.post(controller, newRace);
+
+      // under starter behandling av selve bildet
+      const formData = new FormData();
+      formData.append('formFile', image); // VIKTIG moment her at 'formFile' skal matche med 'formFile' i backend kontroller
+
+      // siste steg, sende bildet til web api'et
+      const uploadResult = await axios({
+        url: imageUploadController,
+        method: 'POST',
+        data: formData,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+
+      console.log(uploadResult);
+
+      //* HUSK DENNE
+      // Vi må kvitte oss med forrige objektet, for så å kunne legge inn et nytt
+      // Denne koden lever konstant under applikasjonen, mens bruker driver å laster opp bilder ogsånt
+      formData.delete('formFile');
     } catch (error) {
       console.log(error);
     }
@@ -34,6 +70,7 @@ const RaceService = (() => {
     getAll,
     getById,
     getByGrandPrix,
+    createNewRace,
   };
 })();
 
